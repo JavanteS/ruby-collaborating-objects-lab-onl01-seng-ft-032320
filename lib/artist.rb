acd91348dfec72f3fe38ced02adb393d9945efdc
Javante Nelson <javantenelson@gmail.com>
2020-04-07 18:41:04 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -29,7 +29,7 @@ class Artist
   def self.find_or_create_by_name(name)
     self.all.find do |singer| if singer.name == name 
     
-  else singer.name = self.new(singer)
+  else Artist.new(singer)
   end
   end
  end
