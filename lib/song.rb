0dfeeab2cb353a47bd1692913de00ec8fcb0b2e6
Javante Nelson <javantenelson@gmail.com>
2020-04-07 20:49:39 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -23,7 +23,7 @@ class Song
    singer = filename.split(" - ")[0]
    song_name = filename.split(" - ")[1]
    song = self.new(song_name)
-   song.artist_name = singer
+   artist = Artist.find_or_create_by_name(artist_name)
    
    
   
